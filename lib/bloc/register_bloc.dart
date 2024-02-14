8703d3d21e683815c3cf2c3bd3a42799c5e3baad
Nebot774 <adriclemenbot@gmail.com>
2024-02-14 19:41:35 +0100

    Commit:"register_bloc.dart lanzar el metodo para registrar el usuario en la base de datos odoo en processo

--- lib/bloc/register_bloc.dart
+++ lib/bloc/register_bloc.dart
@@ -1,63 +1,48 @@
 import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase;
 
 import 'package:firebase_auth/firebase_auth.dart';
+import '../data/odoo_connect.dart';
+import '../model/language.dart';
 
 
 // Eventos relacionados con la autenticación
 abstract class AuthEvent {}
 
-
 class SignUpUser extends AuthEvent {
   final String email;
-
 
   SignUpUser(this.email);
 }
 
-
 class VerifyCode extends AuthEvent {
   final String verificationCode;
-
 
   VerifyCode(this.verificationCode);
 }
 
-
 // Estados de autenticación
 abstract class AuthState {}
 
-
 class AuthenticationLoadingState extends AuthState {}
-
 
 class AuthenticationSuccessState extends AuthState {
   final User user;
 
-
   AuthenticationSuccessState(this.user);
 }
-
 
 class AuthenticationFailureState extends AuthState {
   final String errorMessage;
 
-
   AuthenticationFailureState(this.errorMessage);
 }
-
 
 class AuthenticationBloc extends Bloc<AuthEvent, AuthState> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-
-  // Constructor actualizado
   AuthenticationBloc() : super(AuthenticationLoadingState());
-
-
-  @override
-  AuthState get initialState => AuthenticationLoadingState();
-
 
   @override
   Stream<AuthState> mapEventToState(AuthEvent event) async* {
@@ -68,8 +53,8 @@ class AuthenticationBloc extends Bloc<AuthEvent, AuthState> {
         await _auth.sendSignInLinkToEmail(
           email: event.email,
           actionCodeSettings: ActionCodeSettings(
-            url: 'https://your-app-url.com', // URL profunda para redirigir después de la verificación
-            handleCodeInApp: true, // Abre la aplicación automáticamente al hacer clic en el enlace
+            url: 'https://your-app-url.com',
+            handleCodeInApp: true,
           ),
         );
         yield AuthenticationSuccessState(_auth.currentUser!);
@@ -81,14 +66,41 @@ class AuthenticationBloc extends Bloc<AuthEvent, AuthState> {
       try {
         final AuthCredential credential = EmailAuthProvider.credentialWithLink(
           email: _auth.currentUser!.email!,
-          emailLink: event.verificationCode, // Cambia 'link' a 'emailLink'
+          emailLink: event.verificationCode,
         );
         final UserCredential userCredential =
         await _auth.signInWithCredential(credential);
-        yield AuthenticationSuccessState(userCredential.user!);
+
+        // Verificamos que el usuario no sea nulo antes de llamar a createUser
+        if (userCredential.user != null) {
+          // Crear una instancia de tu clase User usando la información del User de Firebase
+          User myAppUser = User(
+            userCredential.user!.email!, // asegúrate de que el email no sea nulo
+            '', // password - puede ser cualquier valor, ya que no se usa
+            true, // active
+            '', // name - puede ser cualquier valor, ya que no se usa
+            //Language.english, // lang
+            userCredential.user!.uid, // id
+            '', // avatar - puede ser cualquier valor, ya que no se usa
+          );
+
+          final bool isCreated = await OdooConnect.createUser(myAppUser);
+          if (isCreated) {
+            yield AuthenticationSuccessState(userCredential.user!);
+          } else {
+            yield AuthenticationFailureState('Error al registrar usuario en Odoo');
+          }
+        } else {
+          yield AuthenticationFailureState('Error al obtener datos del usuario');
+        }
+
+        // Añadimos la línea para asignar firebase.User a firebaseUser
+        firebase.User firebaseUser = userCredential.user!;
       } catch (e) {
         yield AuthenticationFailureState(e.toString());
       }
     }
   }
 }
+
+
